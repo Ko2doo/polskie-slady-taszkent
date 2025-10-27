427d2af237aa94651e86116004718cfe48a1c1b7
Ko2doo <galaxyrobotix@gmail.com>
2025-10-27 17:51:43 +0500

    UPD:2025-10-27 Starting localization

--- src/locales/pl/articles.js
+++ src/locales/pl/articles.js
@@ -1,5 +1,36 @@
-// Articles locales
+/**
+ * Article locales
+ *
+ * idName: {
+ *    title: {...},
+ *    description: {...}
+ * }
+ */
 
 export default {
-  test: {},
+  l01: {
+    title: 'Ambasada RP W Taszkence',
+    description:
+      'Rzeczpospolita Polska uznala niepodległość Republiki Uzbekistanu w grudniu 1991 roku. Stosunki dyplomatyczne oba kraje nawiązały w lutym 1992 roku. W 1998 r. Taszkencie powstała polska Ambasada, która na początku mieściła się przy ulicy Miranszoh, następnie zaś przy ul Firdavsiy 66 (gdzie znajduje się do dzisiaj). Pierwszym Ambasadorem RP w Uzbekistanie był Andrzej Kaczorowski (1998-2001), jego następcą został Ambasador Zenon Kuchciak (2001-2005). Po nim placówką kierowali kolejni dyplomaci w randze chargę d`affaires: Janusz Wawrzyniuk (2005-2006), Marian Orlikowski (2006-2007) i Jerzy Stankiewicz (2007-2011). Od lutego 2011 roku na czele plaćowki stanął Ambasador - Marian Przeździecki, który pełnił swą misję do lata 2015 roku. Pod koniec tegoż roku przybył do Taszkentu jego następca - Ambasador Piotr Iwaszkiewicz, który kierował Ambasadą w latach (2015-2020). Następnie został wyznaczony chargé d`affaires Tomasz Kollat (2020-2021). A w 2021 roku na stanowisko Ambasadora został mianowany Radosław Gruk (2021-2024)',
+  },
+  l02: {
+    title: 'Centrum kultury Polskiej w Uzbekistanie "Świetlica Polska"',
+    description:
+      'Polskie Centrum Kultury Uzbekistanu „Świetlica Polska” – organizacja polonijna – jest pozarządową i niedochodową organizacją zrzeszającą mieszkańców Uzbekistanu polskiego pochodzenia, a także wszystkich przyjaciół Polski – osoby w różnym wieku, różnych narodowości i wyznań. Celem Centrum jest zachowanie i rozwój w Uzbekistanie polskiej kultury narodowej, języka, zwyczajów i tradycji oraz wspieranie zaspokajania potrzeb narodowo-kulturalnych, edukacyjnych i innych potrzeb duchowych. <br><br> Centrum zostało założone w 1989 roku. Pierwszym przewodniczącym Centrum był Aleksander Lewiński, który kierował nim w latach 1989–1994. Drugą przewodniczącą była Irena Tagijewa, pełniąca tę funkcję w latach 1994–2010. Trzecim i obecnym przewodniczącym Centrum jest Oleg Skurydin.',
+  },
+  l03: {
+    title: 'Władysław Januszewski',
+    description:
+      'W 1912 r. przybył na terytorium Turkiestanu, pochodzący z Wilna, cyrkowiec-akrobata, Władysław Januszewski (1867-1970). Występował pod pseudonimem Kadyr Guliam. Jako pierwszy na świecie stworzył uzbecki numer cyrkowy z wytresowanymi wielbłądami. Jego popisowy numer "Akrobaci na wielbłądach" zyskal uznanie w wielu krajach świata, przynosząc sławę uzbeckiej sztuce cyrkowej. Za swe zasługi otrzymał tytuł "Zasłużony Artysta Rosji". Januszewski był człowiekiem o wyjątkowej sile i zręczniści. Znał dorze technikę uzbeckiego zapaśnictwa ludowego. <br><br> Finalową sztuczką każdego występu była piramida. Uczestnicy (od ośmiu do dziewięciu osób) wspinali się na Januszewskiego, a on uśmiechając się, robił piruet i szedł z piramidą za kulisy. Specjaliści wiedzą, że można utrzymać duża wagę, stojac w jednym miejscu, ale chodzenie z nią jest już wyjątkową sztuka. Władysław Januszewski pracował w starym taszkenckim cyrku Jupatowa, który stał przy Niedzielnym Bazarze, w miejscu, gdzie obecnie znajduje się Państwowe Muzeum Historii Uzbekistanu.',
+  },
+  l04: {
+    title: 'Anna German',
+    description:
+      'Anna German (1936-1982) - wielka polska piosenkarka urodziła się na uzbeckiej ziemi, w mieście Urgencz, 14 lutego 1936 roku. kiedy miała póltora roku, ciężko zachorowała i rodzice zawieźli ją do lekarza w Taszkencie, gdzie po aresztowaniu ojca Anny, zamieszkała wraz z mamą, babcią i ciocią. Często chodziły na spacer do parku imienia Puszkina, który istnieje do dziś i nose nazwę Abdulla Kadiri. A tuż obok parku stoi Szkoła nr 42, w której nauczycielką jęnzyka niemieckiego była mama piosenkarki - Irma Martens, która pracowała również na Uniwersytecie Pedagogicznym, gdzie także uczyła niemieckiego. Anna German zawsze z ciepłem i miłością wspominała swoja ojczyznę. W polskim domu piosenkarki goście pili herbatę tylko z uzbeckich pialuszek, a sama gospodyni gotowała wspaniały płow.',
+  },
+  l05: {
+    title: 'Anna Drżewicka "Ałmatyńska"',
+    description:
+      'Na początku współczesnej ulicy Mustakillik stoi pięnkny budynek, w którym po II wojnie światowej mieściła się siedziba Związku Pisarzy Uzbekistanu. W tym czasie Anna Drżewicka "Ałmatyńska" (1883-1973) była jedną z najstarszych pisarek tego regionu i współzałożycielką swiązku. <br><br> "Ałmatyńska" to jej pseudonim. Wybrała go, ponieważ urodziła się w 1883 r. w mieście Ałmata. Jej ojciec, Waldemar Drżewicki, był polskim patriotą, który został ukarany za udział w powstaniu styczniowym w 1863 r. dożywotnim zesłaniem na terytorium Turkiestanu. Na początku XX wieku autorka napisała wiele poetyckich i prozaicznych dzieł, ale najbardziej znana była jej powieść w trzech tomach pt. "Ucisk" o wydarzeniach, które miały miejsce w tym szasie w Turkiestanie. Anna "Ałmatyńska" Drżewicka zmarła w sierpniu 1973 r. w Taszkencie.',
+  },
 };
